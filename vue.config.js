a6a512341a6bc9ad2daa76b40ce75b40f28218c0
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2022-01-24 16:50:50 +0900

    Modified publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   outputDir: 'docs',
   assetsDir: './',
-  publicPath: './',
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/Online_Dating_App/' : '/',
   transpileDependencies: ['vuetify'],
 };
